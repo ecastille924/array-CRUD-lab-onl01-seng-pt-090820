82db3360c68bfbacd00f3a4a4733896998c3f7a4
Erick Castille <ecastille924@gmail.com>
2020-09-13 20:12:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  more_words = ["wow", "super", "yippee"]
+  more_words[0]
 end
 
 def retrieve_last_element_from_array(array)
