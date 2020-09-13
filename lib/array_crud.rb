8399c2311ad4ac11c538058eda4e46814f22d40e
Erick Castille <ecastille924@gmail.com>
2020-09-13 19:53:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   taco_toppings = ["cheese", "tomato", "onion"]
-  taco_toppings.unshift ("jalepeno")
+  taco_toppings.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
