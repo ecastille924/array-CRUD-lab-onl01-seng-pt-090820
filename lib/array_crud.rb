9374cee8c9dd39dec3d05018235a9f7d4b131ebf
Erick Castille <ecastille924@gmail.com>
2020-09-13 20:03:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  stuff_learned = ["strings", "integers", "arrays!"]
+  stuff_learned = ["strings", "integers", "arrays"]
   arrays = stuff_leanred.pop 
   
 end
