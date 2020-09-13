b41c9dc122e1a690eda4a72a39b39c23ae364db9
Erick Castille <ecastille924@gmail.com>
2020-09-13 20:10:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  stuff = ["chair", "computer", "plant"]
-  stuff [1]
+  words = ["the", "am", "plant"]
+  words [1]
 end
 
 def retrieve_first_element_from_array(array)
