6bfa1f5b680a9020e25463643e1b3346618e37c8
Erick Castille <ecastille924@gmail.com>
2020-09-13 19:48:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   j_names = ["Joe", "John", "Jill", "Jenna"]
   j_names << "Jennifer"
-  puts j_names.inspect
+  puts j_names
 end
 
 def add_element_to_start_of_array(array, element)
