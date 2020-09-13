691e6c323ac981b92c27c27b5634f7c11b2e1e63
Erick Castille <ecastille924@gmail.com>
2020-09-13 19:45:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  J_names = ["Joe", "John", "Jill", "Jenna"]
-  J_names << "Jennifer"
+  ["Joe", "John", "Jill", "Jenna"]
+  << "Jennifer"
   
 end
 
